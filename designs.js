b2e094673b0a27842cb3e11cbe261fc64ccb096e
DeeDee <diatilel@gmail.com>
2018-07-01 09:08:39 +0200

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,50 @@
+
+
+
+ // When size is submitted and color picked by the user, call makeGrid() and colorIn() 
+ $('#sizePicker').submit(function(evt){
+	 evt.preventDefault();
+	 $('#pixelCanvas').children().remove();
+	 makeGrid();
+	 colorIn();
+ });
+ 
+ // Select size input 
+ function makeGrid() { 
+	var row;
+	var column;
+	var table;
+	
+	row = $('#inputWidth').val();
+	column =$('#inputHeight').val();
+	table = $('#pixelCanvas');
+	
+ // Create rows  
+	for (var x =1; x <= row; x++){
+		table.append('<tr></tr>');
+		
+		//Create columns
+		for(var y =1; y <= column; y++){
+			$('tr').last().append('<td></td>');
+		}
+	}
+ } 
+
+ //Select a color input
+function colorIn(){
+	var colorSelector;
+	colorSelector = $('#colorPicker').val();
+	$('td').click(function(){
+		$(this).css('background',colorSelector);
+	});
+	//Remove color from grid when double click cell
+	$('td').dblclick(function(evt){
+		evt.preventDefault();
+		$(this).css('background-color','transparent');
+
+	});
+}
+
+ 
+ 
+ 
